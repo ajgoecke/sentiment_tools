8e914afa39fa17f3181bb763f93e23cb96ff0f4e
FriederikeSch <58273193+FriederikeSch@users.noreply.github.com>
2022-03-08 15:38:34 +0100

    Delete test1.R

--- test1.R
+++
@@ -1,6 +0,0 @@
-library(R.utils)
-library("rjson")
-setwd("/Users/rike-/Documents/Studium/3 Semester/PM/Corpora")
-dt<-R.utils::gunzip("goodreads_reviews_children.json.gz", remove=FALSE)
-result <- fromJSON(file = "goodreads_reviews_children.json.gz")
-
